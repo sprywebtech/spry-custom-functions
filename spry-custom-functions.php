fc4a29d9f0d07fdb962c4b4a0b2d04df5ef366b8
sprywebtech <dougmorris06@gmail.com>
2025-04-17 10:05:16 -0500

    updated header information to include author, license, and WordPress and PHP version requirements

--- spry-custom-functions.php
+++ spry-custom-functions.php
@@ -2,9 +2,13 @@
 /*
 Plugin Name: Spry Custom Functions
 Description: Simple, small, and lightweight WordPress plugin that allows you to enter custom functions just like you would use functions.php in a theme. This solves the issue of not having a functions.php file for some page builders. There is no admin interface, so adding custom functions works just like adding them to functions.php via a code editor.
-Version: 1.0.1
+Version: 1.0.2
+Requires at least: 5.2
+Requires PHP: 7.4
 Author: Spry Web Tech
 Author URI: https://sprywebtech.com
+License: GPL v2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
