87a5f6597c04fc652b7b0486bf05e6cd9787ac2c
sprywebtech <dougmorris06@gmail.com>
2025-04-17 07:41:48 -0500

    Adding repository

---
+++ spry-custom-functions.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Spry Custom Functions
+Description: Custom website functions used on all sites. 
+Version: 1.1
+Author: Doug Morris
+*/
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
+/* Add custom functions below this line
+========================================= */
+
